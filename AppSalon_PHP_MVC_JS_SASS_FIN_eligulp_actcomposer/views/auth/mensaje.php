3e46b7e4055ff3095d02852578a3c5f6cb9fd02f
Ernesto <jorge.cywdt@gmail.com>
2023-04-26 19:42:32 -0500

    Init commit

---
+++ AppSalon_PHP_MVC_JS_SASS_FIN_eligulp_actcomposer/views/auth/mensaje.php
@@ -0,0 +1,5 @@
+<h1 class="nombre-pagina">Confirma tu cuenta</h1>
+
+<p class="descripcion-pagina">Hemos enviado las instrucciones para confirmar tu cuenta a tu e-mail.</p>
+
+<a class="boton" style="margin-right: 3%;" href="/">Regresar</a>
